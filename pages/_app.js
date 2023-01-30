db1233ad20aa22d75b3c78edf908c5f81911b36d
jaypowar00 <jaypowar00@gmail.com>
2023-01-30 22:17:03 +0530

    [+] added google-site-verification

--- pages/_app.js
+++ pages/_app.js
@@ -7,25 +7,28 @@ import { ShareModalWrapper } from '@/context/ShareModalContext'
 import { Toaster } from 'react-hot-toast'
 import TimeAgo from 'javascript-time-ago'
 import en from 'javascript-time-ago/locale/en'
-try{
+import Head from 'next/head'
+try {
   TimeAgo.addDefaultLocale(en)
-}catch(e){
+} catch (e) {
   console.log(e)
 }
-
 export default function App({ Component, pageProps }) {
   const router = useRouter()
   // console.log(router.basePath)
   return (
-  <ShareModalWrapper>
-    <ShareModal/>
-    <div className='flex flex-row'>
-      <SideBar />
-      <div className='main-container'>
-        <Component {...pageProps} />
+    <ShareModalWrapper>
+      <Head>
+        <meta name="google-site-verification" content="LsSs0Uu_szcBdSa0kLK21udIohiOBy14mBEdJ0w4T3c" />
+      </Head>
+      <ShareModal />
+      <div className='flex flex-row'>
+        <SideBar />
+        <div className='main-container'>
+          <Component {...pageProps} />
+        </div>
       </div>
-    </div>
-    <Toaster position='bottom-right' toastOptions={{duration: 2000}} reverseOrder={false} />
-  </ShareModalWrapper>
+      <Toaster position='bottom-right' toastOptions={{ duration: 2000 }} reverseOrder={false} />
+    </ShareModalWrapper>
   )
 }
