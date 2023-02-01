312b99682c05c2f750e544ca7162243381ab4df3
jaypowar00 <jaypowar00@gmail.com>
2023-02-02 00:36:43 +0530

    [+] fixed useEffect issue for disqus component

--- components/DisqusComments.js
+++ components/DisqusComments.js
@@ -1,18 +1,12 @@
 import { DiscussionEmbed } from "disqus-react"
-import { useEffect, useState } from "react"
 
-const DisqusComments = ({ article }) => {
+const DisqusComments = ({ article, hostUrl }) => {
     const disqusShortname = "csgeeks"
-    const [hostUrl, setHostUrl] = useState("https://csgeeksblog.netlify.app")
     const disqusConfig = {
         url: `${hostUrl}/posts/${article._id}`,
         identifier: article._id, // Single article id
         title: article.title // Single article title
     }
-    useEffect(() => {
-        if (window.location.origin !== hostUrl)
-            setHostUrl(window.location.origin)
-    }, [])
 
     return (
         <div>
