7ebe8615590b6899326283bd279644d8939734ea
jaypowar00 <jaypowar00@gmail.com>
2023-02-02 18:38:06 +0530

    [+] auto detect sidebar minimized state

--- context/ShareModalContext.js
+++ context/ShareModalContext.js
@@ -1,10 +1,15 @@
-const { createContext, useContext, useState } = require("react");
+const { createContext, useContext, useState, useEffect } = require("react");
 
 const ShareModalContext = createContext()
 
-export const ShareContextWrapper = ({children}) => {
+export const ShareContextWrapper = ({ children }) => {
     const [modalShareOpen, setModalShareOpen] = useState(false)
     const [sidebarMinimize, setSidebarMinimize] = useState(false)
+    useEffect(() => {
+        if (localStorage.getItem('minimized'))
+            setSidebarMinimize(localStorage.getItem('minimized') === 'true')
+    }, [])
+
     return (
         <ShareModalContext.Provider value={{
             modalShareOpen, setModalShareOpen,
