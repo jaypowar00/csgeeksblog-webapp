c8d1faeec4dcf955e0916114d3d710df2b633554
jaypowar00 <jaypowar00@gmail.com>
2023-02-02 14:31:50 +0530

    [-] removed unused getStaticProps

--- components/PostArticleItem.js
+++ components/PostArticleItem.js
@@ -64,18 +64,3 @@ function PostArticleItem({
 }
 
 export default PostArticleItem;
-
-// You should use getStaticProps when:
-//- The data required to render the page is available at build time ahead of a user’s request.
-//- The data comes from a headless CMS.
-//- The data can be publicly cached (not user-specific).
-//- The page must be pre-rendered (for SEO) and be very fast — getStaticProps generates HTML and JSON files, both of which can be cached by a CDN for performance.
-export const getStaticProps = async (ctx) => {
-    console.log(ctx)
-
-    return {
-        props: {
-
-        }
-    }
-}
