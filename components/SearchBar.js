36d492666262dc2d68eb828d39bb09df1ffc8268
jaypowar00 <jaypowar00@gmail.com>
2023-02-11 18:03:49 +0530

    [+] fixed warning related react attribute renaming

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -63,10 +63,10 @@ function FullSearchBar() {
             <div className="absolute z-20 w-full top-[24px] max-w-[700px] left-[50%] translate-x-[-50%] px-8">
                 <form className="flex items-center" onSubmit={e => { e.preventDefault(); }}>
                     {/* <form className="flex items-center" onSubmit={e => { e.preventDefault(); getSearchedItems() }}> */}
-                    <label for="simple-search" className="sr-only">Search</label>
+                    <label htmlFor="simple-search" className="sr-only">Search</label>
                     <div className="relative w-full">
                         <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clipRule="evenodd"></path></svg>
                         </div>
                         <input type="search" id="simple-search" className="bg-gray-50 border-0 outline-none text-sm rounded-lg block w-full pl-10 p-2.5 bg-gray-700 placeholder-gray-400 text-white ring-[1.5px] ring-green-600" placeholder="Search articles" required
                             ref={inputRef} value={searchString} onChange={onSearchStringChange} />
